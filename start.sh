0ad0cc7ad691400df6d81f5a703ebbfc29bcc9ed
helxkc <116129102+helxkc@users.noreply.github.com>
2022-10-18 20:58:44 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=ba3f6e6d-0648-4b39-b35d-36d2355e719
+export UUID=390ce304-abeb-4e77-8647-01710a203237
 
 #请修改为自己设置的伪装站，不要带https://
 export ProxySite=www.holehike.com
